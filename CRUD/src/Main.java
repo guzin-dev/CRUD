40200d07dd441fa4e059739d834c5212e60d4543
guzin-dev <guzinhojk99@gmail.com>
2022-09-29 20:17:27 -0300

    Código finalizado

--- CRUD/src/Main.java
+++ CRUD/src/Main.java
@@ -4,7 +4,6 @@ import java.util.*;
 public class Main {
 
     static ArrayList<Pessoa> listaDePessoas = new ArrayList<>();
-    static Scanner leitor = new Scanner(System.in);
     static int id = 0;
 
     public static void main(String[] args) {
@@ -24,7 +23,7 @@ public class Main {
                 ePessoa();
                 break;
             case 4:
-                //dPessoa();
+                dPessoa();
                 break;
             case 5:
                 int resposta = JOptionPane.showConfirmDialog(null, "Tem certeza que deseja fechar o programa?", "Atenção", JOptionPane.YES_NO_OPTION);
@@ -51,8 +50,8 @@ public class Main {
 
     public static void vPessoas(){
         String pessoas = "";
-        for (int i = 0; i < listaDePessoas.size(); i++) {
-            pessoas += "Nome: " + listaDePessoas.get(i).getNome() + ", idade: " + listaDePessoas.get(i).getIdade() + ", id: " + listaDePessoas.get(i).getId() + "\n";
+        for (Pessoa pessoa : listaDePessoas) {
+            pessoas += "Nome: " + pessoa.getNome() + ", idade: " + pessoa.getIdade() + ", id: " + pessoa.getId() + "\n";
         }
         if(listaDePessoas.isEmpty()){
             JOptionPane.showMessageDialog(null, "Não existem pessoas!");
@@ -63,25 +62,50 @@ public class Main {
     }
 
     public static void ePessoa(){
-        String pessoas = "";
         boolean encontrou = false;
         if(listaDePessoas.isEmpty()){
             JOptionPane.showMessageDialog(null, "Não existem pessoas!");
         }else {
             int idParaPesquisa = Integer.parseInt(JOptionPane.showInputDialog("Insira o id da pessoa que você deseja editar"));
-            for (int i = 0; i < listaDePessoas.size(); i++) {
-                if(listaDePessoas.get(i).getId() == idParaPesquisa){
-                    String novoNome = JOptionPane.showInputDialog("Nome atual da pessoa: " + listaDePessoas.get(i).getNome() + "\nInsira o novo nome para este usuário");
-                    int novaIdade = Integer.parseInt(JOptionPane.showInputDialog("Idade atual da pessoa: " + listaDePessoas.get(i).getIdade() + "\nInsira a nova idade para este usuário"));
-                    listaDePessoas.get(i).setNome(novoNome);
-                    listaDePessoas.get(i).setIdade(novaIdade);
+            for (Pessoa pessoa : listaDePessoas) {
+                if (pessoa.getId() == idParaPesquisa) {
+                    String novoNome = JOptionPane.showInputDialog("Nome atual da pessoa: " + pessoa.getNome() + "\nInsira o novo nome para este usuário");
+                    int novaIdade = Integer.parseInt(JOptionPane.showInputDialog("Idade atual da pessoa: " + pessoa.getIdade() + "\nInsira a nova idade para este usuário"));
+                    pessoa.setNome(novoNome);
+                    pessoa.setIdade(novaIdade);
                     JOptionPane.showMessageDialog(null, "Pessoa editada com sucesso!");
                     encontrou = true;
                 }
             }
+            if(!encontrou){
+                JOptionPane.showMessageDialog(null, "Nenhuma pessoa encontrada com esse id");
+            }
         }
-        if(encontrou == false){
-            JOptionPane.showMessageDialog(null, "Nenhuma pessoa encontrada com esse id");
+        menu();
+    }
+
+    public static void dPessoa(){
+        boolean encontrou = false;
+        if(listaDePessoas.isEmpty()){
+            JOptionPane.showMessageDialog(null, "Não existem pessoas!");
+        }else {
+            int idParaPesquisa = Integer.parseInt(JOptionPane.showInputDialog("Insira o id da pessoa que você deseja editar"));
+            for (Pessoa pessoa : listaDePessoas) {
+                if(pessoa.getId() == idParaPesquisa){
+                    int resposta = JOptionPane.showConfirmDialog(null, "Nome: " + pessoa.getNome() + ", idade: "+ pessoa.getIdade() + "\nTem certeza que deseja deletar esta pessoa?", "Atenção", JOptionPane.YES_NO_OPTION);
+                    if (resposta == JOptionPane.YES_OPTION) {
+                        listaDePessoas.remove(pessoa);
+                        JOptionPane.showMessageDialog(null, "Pessoa deletada com sucesso!");
+                        menu();
+                    } else {
+                        menu();
+                    }
+                    encontrou = true;
+                }
+            }
+            if(!encontrou){
+                JOptionPane.showMessageDialog(null, "Nenhuma pessoa encontrada com esse id");
+            }
         }
         menu();
     }
